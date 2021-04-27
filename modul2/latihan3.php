2af9ad65e0dd1344b966ac31ea6b0ee3d91303c0
ivansyahputra1 <brgkicaumania@gmail.com>
2021-04-27 22:44:57 +0700

    project2

---
+++ modul2/latihan3.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Contoh Program Aritmatika dengan PHP</title>
+</head>
+<body>
+<center>Program Sederhana Aritmatika</center>
+<form action="aksi.php" method="post">
+Nilai Pertama <input type="text" name="satu" Placeholder="Masukan Nilai Pertama" ><br/>
+<select name="operator">
+<option>+</option>
+<option>-</option>
+<option>*</option>
+<option>/</option>
+</select>
+Nilai Kedua <input type="text" name="dua" Placeholder="Masukan Nilai Kedua"><br/>
+<input type="submit" Value="Submit">
+</form>
+</body>
+</html>
